bd807d80a8a5a2af6e09e5d305c0a960131fdd09
Anubhav Gupta <anubhavgupta916@gmail.com>
2025-06-29 18:26:47 +0530

    while & do-while

---
+++ 05_iterations/while.js
@@ -0,0 +1,31 @@
+// while
+let index = 0
+while (index <= 10) {
+    console.log(`The value of Index of ${index}`);
+    index++
+}
+
+let myArray = ["Ironman", "superman", "batman"]
+
+let arr = 0 
+
+while (arr < myArray.length) {
+    console.log(`The value is ${myArray[arr]}`)
+    arr++
+}
+
+//==================================== Do-while ==========================================
+// the loop will run atleast one time.
+
+let i = 1
+do {
+    console.log(`The value is ${i}`);
+    i++
+} while (i <= 10);
+
+
+let j = 11
+do {
+    console.log(`The value is ${j}`);
+    j++
+} while (j <= 10);
