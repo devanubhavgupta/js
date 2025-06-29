d8ea08b237d22ad2459cea52433bcc7a6b38adfa
Anubhav Gupta <anubhavgupta916@gmail.com>
2025-06-29 13:37:17 +0530

    switch case

---
+++ 04_controlFlow/switch.js
@@ -0,0 +1,54 @@
+// ====================================Basic Syntax==========================================//
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// ============================================================================================//
+
+// const month = 2
+// switch (month) {
+//     case 1:
+//         console.log(`Januaray`);
+        
+//         break;
+//     case 2:
+//         console.log(`February`);
+        
+//         break;
+//     case 3:
+//         console.log(`March`);
+    
+//         break;
+//     default:
+//         console.log(`Default value matched`);
+        
+//         break;
+// }
+
+//=========================================================================================//
+
+const month = "Two"
+switch (month) {
+    case "One":
+        console.log(1);
+        
+        break;
+    case "Two":
+        console.log(2);
+        
+        break;
+    case "Three":
+        console.log(3);
+    
+        break;
+    default:
+        console.log(`Default value matched`);
+        
+        break;
+}
